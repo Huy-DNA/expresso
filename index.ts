323ea5bc256dbd384a5eb234ac97a9de429c5673
HuyDNA <huydo862003@gmail.com>
2024-11-30 10:48:08 +0700

    feat: fill basic information (except body) in a Request

--- index.ts
+++ index.ts
@@ -1,5 +1,43 @@
 import http from "node:http";
 import type { IncomingMessage, Server, ServerResponse } from "node:http";
+import * as cookie from "npm:cookie@1.0.2";
+import qs from "https://deno.land/x/deno_qs/mod.ts";
+
+class Request {
+  private req: IncomingMessage;
+  private app: ExpressoApp;
+ 
+  readonly body?: unknown; // filled outside of the contructor
+  readonly cookies: Readonly<Record<string, string | undefined>>;
+  readonly host: string;
+  readonly hostname: string;
+  readonly ip?: string;
+  readonly method: HttpMethod;
+  readonly path: string;
+  readonly query: Readonly<Record<string, string>>;
+
+  constructor (app: ExpressoApp, req: IncomingMessage) {
+    this.app = app;
+    this.req = req;
+
+    const { headers } = req;
+
+    this.cookies = !headers.cookie ? {} : cookie.parse(headers.cookie);
+
+    this.host = headers.host || "";
+    this.hostname = headers.host || "";
+
+    this.method = req.method as HttpMethod;
+
+    const url = new URL(`http://expresso.com/${headers.host}`);
+    this.path = url.pathname;
+    this.query = qs.parse(url.search);
+
+    this.ip = req.socket.remoteAddress;
+  }
+}
+
+export { type Request };
 
 export enum HttpMethod {
   GET = "GET",
@@ -17,13 +55,14 @@ class ExpressoApp {
 
   constructor () {
     this.server = http.createServer(
-      (req: IncomingMessage, res: ServerResponse) => {
+      (_req: IncomingMessage, _res: ServerResponse) => {
+        const req = new Request(this, _req);
         const body = "hello world";
-        res.writeHead(200, {
+        _res.writeHead(200, {
           "Content-Type": "text/plain",
           "Content-Length": body.length,
         });
-        res.end(body);
+        _res.end(body);
       },
     );
   }
