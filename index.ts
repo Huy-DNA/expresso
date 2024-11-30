c843837e7072d188176267665715df6f091bae5b
HuyDNA <huydo862003@gmail.com>
2024-11-30 11:37:37 +0700

    feat: send(), end(), status() methods on Response

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import http from "node:http";
 import type { IncomingMessage, Server, ServerResponse } from "node:http";
 import * as cookie from "npm:cookie@1.0.2";
 import qs from "https://deno.land/x/deno_qs/mod.ts";
+import { Buffer } from "node:buffer";
 
 class Request {
   private req: IncomingMessage;
@@ -39,14 +40,53 @@ class Request {
 
 export { type Request };
 
+type JsonConvertible = string | boolean | number | undefined | null | JsonArray | JsonObject;
+interface JsonArray extends Array<JsonConvertible> {};
+interface JsonObject extends Record<string, JsonConvertible> {};
+
 class Response {
   private res;
+  private isEnded: boolean;
   
   readonly app: ExpressoApp;
+
+  private statusCode: number | undefined;  
+  private body: Buffer | string | undefined;
+  private contentType: string | undefined;
+  private get contentLength (): number {
+    return this.body ? this.body.length : 0;
+  }
 
   constructor (app: ExpressoApp, res: ServerResponse) {
     this.app = app;
     this.res = res;
+    this.isEnded = false;
+  }
+
+  status (code: number): Response {
+    this.statusCode = code;
+    return this;
+  }
+
+  send (data: Buffer | string | JsonConvertible): Response {
+    if (data instanceof Buffer) {
+      this.body = data;
+      this.contentType = "application/octet-stream";
+    } else if (typeof data === 'string') {
+      this.body = data;
+      this.contentType = "text/html";
+    } else {
+      this.body = JSON.stringify(data);
+      this.contentType = "application/json";
+    }
+    return this;
+  }
+
+  end () {
+    if (this.isEnded) return;
+    this.isEnded = true;
+    this.res.writeHead(this.statusCode || 200, { "content-length": this.contentLength, "content-type": this.contentType || 'plain/text' });
+    this.res.end(this.body);
   }
 }
 
@@ -79,12 +119,12 @@ class ExpressoApp {
       async (_req: IncomingMessage, _res: ServerResponse) => {
         const req = new Request(this, _req);
         (req.body as unknown) = await readRequestBody(_req);
-        const body = "hello world";
-        _res.writeHead(200, {
-          "Content-Type": "text/plain",
-          "Content-Length": body.length,
-        });
-        _res.end(body);
+        const res = new Response(this, _res);
+
+        res.send(["Hello World", "This is from expresso"]).end();
+
+        /* Implicitly end the response */
+        res.end();
       },
     );
   }
