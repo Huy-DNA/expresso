bfcc7e2678bfaaff0b774316a04cb7f50306fbb3
HuyDNA <huydo862003@gmail.com>
2024-12-29 18:27:28 +0700

    chore: remove adhoc test code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import { ExpressoApp } from "./app.ts";
-import { bodyParser } from "./plugins/body-parser.ts";
-import { cors } from "./plugins/cors.ts";
-import { serveStatic } from "./plugins/static.ts";
 
 export default function () {
   return new ExpressoApp();
@@ -11,14 +8,3 @@ export type { Request } from "./request.ts";
 export type { Response } from "./response.ts";
 export type { ExpressoApp } from "./app.ts";
 export * from "./plugins/index.ts";
-
-if (import.meta.main) {
-  const app = new ExpressoApp();
-  app.use(cors());
-  app.use(serveStatic("/public", { fallthrough: false }));
-  app.use(bodyParser.json());
-  app.use('/home', (req, res, next) => res.status(200).cookie('id1', '1234', {}).cookie('id2', '1234', {}).send('Hello world!').append('header', ['1', '2']).append('header', ['3', '4']).end());
-  app.use('/back', (req, res) => req.query.confirm ? res.redirect('/home') : res.redirect('back'));
-  app.use('/contact', () => {});
-  app.listen(8000);
-}
